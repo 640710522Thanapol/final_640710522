6438fc2d99b072cdc1d7f452bdbfb99543b15c2d
Thanapol <sutenan_t@silpakorn.edu>
2024-03-24 11:28:31 +0700

    final

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
 import 'api_caller.dart';
 import 'dialog_utils.dart';
 import 'my_list_tile.dart';
 import 'my_text_field.dart';
-import 'package:flutter/material.dart';
-import 'package:dio/dio.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   @override
-  _MyAppState createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: WebTypesScreen(),
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> {
+class WebTypesScreen extends StatefulWidget {
+  @override
+  _WebTypesScreenState createState() => _WebTypesScreenState();
+}
+
+class _WebTypesScreenState extends State<WebTypesScreen> {
   // Define a variable to store the data from the API
-  List<dynamic> _wineList = [];
+  List<dynamic> _webTypes = [];
+  int _selectedIndex = -1; // Index of the selected item
+  List<String> _urls = [];
+  List<Map<String, String>> _submittedData = [];
+
+  // Controllers for text fields
+  TextEditingController _urlTextEditingController = TextEditingController();
+  TextEditingController _detailsTextEditingController = TextEditingController();
 
   @override
   void initState() {
@@ -30,10 +45,13 @@ class _MyAppState extends State<MyApp> {
   Future<void> fetchData() async {
     try {
       // Perform GET request using Dio
-      Response response = await Dio().get('https://api.sampleapis.com/wines/reds');
+      Response response = await Dio().get(
+          'https://cpsu-api-49b593d4e146.herokuapp.com/api/2_2566/final/web_types');
       // Extract the data from the response
       setState(() {
-        _wineList = response.data;
+        _webTypes = response.data;
+        // Initialize URLs list
+        _urls = List.generate(_webTypes.length, (index) => '');
       });
     } catch (error) {
       // Handle any errors that occur during the request
@@ -43,27 +61,182 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Red Wines'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Webby Fondues',
+          style: TextStyle(
+            color: Colors.white,
+          ),
         ),
-        body: Center(
-          child: _wineList.isEmpty
-              ? CircularProgressIndicator() // Show loading indicator while data is being fetched
-              : ListView.builder(
-                  itemCount: _wineList.length,
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      title: Text(_wineList[index]['wine']),
-                      subtitle: Text(_wineList[index]['winery']),
-                      leading: CircleAvatar(
-                        backgroundImage: NetworkImage(_wineList[index]['image']),
-                      ),
+        backgroundColor: const Color.fromARGB(255, 80, 154, 82),
+        centerTitle: true,
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(24.0),
+          child: Padding(
+            padding: EdgeInsets.only(bottom: 8.0),
+            child: Text(
+              'ระบบรายงานเว็บไซต์',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 16.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                MyTextField(
+                  controller: _urlTextEditingController,
+                  hintText: 'Enter URL*',
+                ),
+                MyTextField(
+                  controller: _detailsTextEditingController,
+                  hintText: 'Enter details',
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Center(
+              child: _webTypes.isEmpty
+                  ? CircularProgressIndicator()
+                  : ListView.builder(
+                      itemCount: _webTypes.length,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          leading: CircleAvatar(
+                            backgroundImage:
+                                NetworkImage(_webTypes[index]['image']),
+                          ),
+                          title: Text(_webTypes[index]['title']),
+                          subtitle: Text(_webTypes[index]['subtitle']),
+                          trailing: Radio(
+                            value: index,
+                            groupValue: _selectedIndex,
+                            onChanged: (int? value) {
+                              setState(() {
+                                _selectedIndex = value!;
+                              });
+                            },
+                          ),
+                        );
+                      },
+                    ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextButton.icon(
+              onPressed: () {
+                if (_selectedIndex != -1) {
+                  String selectedTitle = _webTypes[_selectedIndex]['title'];
+                  String selectedURL = _urlTextEditingController.text;
+                  String selectedDetails = _detailsTextEditingController.text;
+                  // Check if URL and web type are filled
+                  if (selectedURL.isNotEmpty && selectedTitle.isNotEmpty) {
+                    // URL and web type are filled, proceed with sending data
+                    // Perform some action with the collected data, such as sending it to a server
+                    // Store submitted data
+                    _submittedData.add({
+                      'Title': selectedTitle,
+                      'URL': selectedURL,
+                      'Details': selectedDetails,
+                    });
+                    // Show success dialog
+                    showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          title: Text('Success'),
+                          content: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'ขอบคุณสำหรับการแจ้งข้อมูล\n\nสถิติการรายงาน\n==========',
+                              ),
+                              SizedBox(height: 10),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: _webTypes.map((webType) {
+                                  final title = webType['title'];
+                                  final count = _submittedData.where((data) => data['Title'] == title).length;
+                                  return Text('$title: $count');
+                                }).toList(),
+                              ),
+                            ],
+                          ),
+                          actions: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                                setState(() {
+                                  _selectedIndex = -1; // Clear selection
+                                  _urlTextEditingController
+                                      .clear(); // Clear URL text field
+                                  _detailsTextEditingController
+                                      .clear(); // Clear details text field
+                                });
+                              },
+                              child: Text('OK'),
+                            ),
+                          ],
+                        );
+                      },
                     );
-                  },
-                ), // Display the fetched data
-        ),
+                  } else {
+                    // URL or web type is empty, show an error message
+                    showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          title: Text('Error'),
+                          content:
+                              Text('Please fill in both URL and web type.'),
+                          actions: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              child: Text('OK'),
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  }
+                } else {
+                  // If no web type is selected, show an error message
+                  showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        title: Text('Error'),
+                        content: Text('ต้องกรอก URL และเลือกประเภทเว็บ'),
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: Text('OK'),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                }
+              },
+              icon: Icon(Icons.send),
+              label: Text('ส่งข้อมูล'),
+            ),
+          ),
+        ],
       ),
     );
   }
